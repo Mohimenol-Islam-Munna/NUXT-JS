b623915fbfb5989fba47beb837d8348d1cbd39a3
Mohimenol-Islam-Munna <munna.cse.pust@gmail.com>
2022-06-29 13:02:27 +0600

    middleware context

--- nuxt-complete-practice/npx-installation/pages/index.vue
+++ nuxt-complete-practice/npx-installation/pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h2 style="text-align: center">Nuxt Js Tutorial</h2>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
